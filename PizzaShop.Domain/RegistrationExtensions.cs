e8ae806101acec4b8ed842ff7f2b1af4dde7e11e
Oleh Nyshchyn <nischin0123@gmail.com>
2023-03-21 15:52:55 +0200

    Created RegistrationExtensions

---
+++ PizzaShop.Domain/RegistrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PizzaShop.Domain.Services;
+using PizzaShop.Domain.Interfaces;
+
+namespace PizzaShop.Domain
+{
+    public static class RegistrationExtensions
+    {
+        public static void AddDomain(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IPizzaService, PizzaService>();
+            serviceCollection.AddScoped<IIngredientService, IngredientService>();
+        }
+    }
+}
